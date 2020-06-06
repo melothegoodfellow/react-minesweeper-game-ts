b460b046a8a9db0941331734c65e92370c47306e
Melroy <melroy.fernz@gmail.com>
2020-06-06 17:21:35 +0530

    Updates

---
+++ precache-manifest.fe3455a8305cd8257f40961b99d0c30d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025791b2481bfd8589e0fcba3c498986",
+    "url": "/minesweeper-game/index.html"
+  },
+  {
+    "revision": "7fb79c6665901637fdc7",
+    "url": "/minesweeper-game/static/css/main.34114528.chunk.css"
+  },
+  {
+    "revision": "a77381470fc3e5085ff5",
+    "url": "/minesweeper-game/static/js/2.9d22f05d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper-game/static/js/2.9d22f05d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb79c6665901637fdc7",
+    "url": "/minesweeper-game/static/js/main.af0cd620.chunk.js"
+  },
+  {
+    "revision": "136874c4ace5ea2e1685",
+    "url": "/minesweeper-game/static/js/runtime-main.bc261176.js"
+  }
+]);
